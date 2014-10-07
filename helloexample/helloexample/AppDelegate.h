2fb5fe624f82a691aea293a9df09353f4eda9ab9
Tackyon <tackyonltd@gmail.com>
2014-10-07 10:00:13 -0600

    test

---
+++ helloexample/helloexample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloexample
+//
+//  Created by Supriya Dinakaran on 2014-10-07.
+//  Copyright (c) 2014 Tackyon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloexample/helloexample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloexample
+//
+//  Created by Supriya Dinakaran on 2014-10-07.
+//  Copyright (c) 2014 Tackyon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
